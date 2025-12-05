78ff7be53741b2f4415b0644dc26ef7f23fcc748
Jaeho Cho <jaeho2025@gmail.com>
2025-12-05 11:08:39 -0500

    update nuclio

--- up.sh
+++ up.sh
@@ -2,5 +2,6 @@
 export CVAT_HOST=cvat.jaehho.com
 
 docker compose -f docker-compose.yml \
+    -f components/serverless/docker-compose.serverless.yml \
     -f docker-compose.settings_overlay.local.yml \
     up -d
